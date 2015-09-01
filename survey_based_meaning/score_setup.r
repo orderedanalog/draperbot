1672d2b2e7eece54627e9446f5f356fd0c9de63a
Jay Wilson <Jay Wilson>
2015-09-01 00:15:43 -0400

    undocumented R code additions

---
+++ survey_based_meaning/score_setup.r
@@ -0,0 +1,257 @@
+mysourcedata<-WV6_Data_v_2015_04_18[,c("V2","V4","V5","V6","V7","V8","V9","V12","V13","V14","V15","V16","V17","V18","V19","V20","V21","V22","V66","V70","V71","V72","V73","V74","V74B","V75","V76","V77","V78","V79","V182")]
+mysourcedata[mysourcedata<0]<-0
+
+mysourcedata[mysourcedata==8]<-"Albania"
+mysourcedata[mysourcedata==12]<-"Algeria"
+mysourcedata[mysourcedata==16]<-"American Samoa"
+mysourcedata[mysourcedata==20]<-"Andorra"
+mysourcedata[mysourcedata==24]<-"Angola"
+mysourcedata[mysourcedata==28]<-"Antigua"
+mysourcedata[mysourcedata==31]<-"Azerbaijan"
+mysourcedata[mysourcedata==32]<-"Argentina"
+mysourcedata[mysourcedata==36]<-"Australia"
+mysourcedata[mysourcedata==40]<-"Austria"
+mysourcedata[mysourcedata==48]<-"Bahrain"
+mysourcedata[mysourcedata==50]<-"Bangladesh"
+mysourcedata[mysourcedata==51]<-"Armenia"
+mysourcedata[mysourcedata==52]<-"Barbados"
+mysourcedata[mysourcedata==56]<-"Belgium"
+mysourcedata[mysourcedata==60]<-"Bermuda"
+mysourcedata[mysourcedata==64]<-"Bhutan"
+mysourcedata[mysourcedata==68]<-"Bolivia"
+mysourcedata[mysourcedata==70]<-"Bosnia"
+mysourcedata[mysourcedata==72]<-"Botswana"
+mysourcedata[mysourcedata==76]<-"Brazil"
+mysourcedata[mysourcedata==84]<-"Belize"
+mysourcedata[mysourcedata==100]<-"Bulgaria"
+mysourcedata[mysourcedata==104]<-"Myanmar"
+mysourcedata[mysourcedata==108]<-"Burundi"
+mysourcedata[mysourcedata==112]<-"Belarus"
+mysourcedata[mysourcedata==116]<-"Cambodia"
+mysourcedata[mysourcedata==120]<-"Cameroon"
+mysourcedata[mysourcedata==124]<-"Canada"
+mysourcedata[mysourcedata==144]<-"Sri Lanka"
+mysourcedata[mysourcedata==148]<-"Chad"
+mysourcedata[mysourcedata==152]<-"Chile"
+mysourcedata[mysourcedata==156]<-"China"
+mysourcedata[mysourcedata==158]<-"Taiwan"
+mysourcedata[mysourcedata==170]<-"Colombia"
+mysourcedata[mysourcedata==180]<-"D.R. Congo"
+mysourcedata[mysourcedata==184]<-"Cook Islands"
+mysourcedata[mysourcedata==188]<-"Costa Rica"
+mysourcedata[mysourcedata==191]<-"Croatia"
+mysourcedata[mysourcedata==192]<-"Cuba"
+mysourcedata[mysourcedata==196]<-"Cyprus (G)"
+mysourcedata[mysourcedata==197]<-"Cyprus (T)"
+mysourcedata[mysourcedata==203]<-"Czech Rep."
+mysourcedata[mysourcedata==208]<-"Denmark"
+mysourcedata[mysourcedata==214]<-"Dominican Rep."
+mysourcedata[mysourcedata==218]<-"Ecuador"
+mysourcedata[mysourcedata==222]<-"El Salvador"
+mysourcedata[mysourcedata==226]<-"Eq.Guinea"
+mysourcedata[mysourcedata==231]<-"Ethiopia"
+mysourcedata[mysourcedata==232]<-"Eritrea"
+mysourcedata[mysourcedata==233]<-"Estonia"
+mysourcedata[mysourcedata==246]<-"Finland"
+mysourcedata[mysourcedata==250]<-"France"
+mysourcedata[mysourcedata==268]<-"Georgia"
+mysourcedata[mysourcedata==270]<-"Gambia"
+mysourcedata[mysourcedata==275]<-"Palestine"
+mysourcedata[mysourcedata==276]<-"Germany"
+mysourcedata[mysourcedata==288]<-"Ghana"
+mysourcedata[mysourcedata==292]<-"Gibraltar"
+mysourcedata[mysourcedata==300]<-"Greece"
+mysourcedata[mysourcedata==320]<-"Guatemala"
+mysourcedata[mysourcedata==324]<-"Guinea"
+mysourcedata[mysourcedata==328]<-"Guyana"
+mysourcedata[mysourcedata==332]<-"Haiti"
+mysourcedata[mysourcedata==340]<-"Honduras"
+mysourcedata[mysourcedata==344]<-"Hong Kong"
+mysourcedata[mysourcedata==348]<-"Hungary"
+mysourcedata[mysourcedata==352]<-"Iceland"
+mysourcedata[mysourcedata==356]<-"India"
+mysourcedata[mysourcedata==360]<-"Indonesia"
+mysourcedata[mysourcedata==364]<-"Iran"
+mysourcedata[mysourcedata==368]<-"Iraq"
+mysourcedata[mysourcedata==372]<-"Ireland"
+mysourcedata[mysourcedata==376]<-"Israel"
+mysourcedata[mysourcedata==380]<-"Italy"
+mysourcedata[mysourcedata==384]<-"Côte d'Ivoire"
+mysourcedata[mysourcedata==388]<-"Jamaica"
+mysourcedata[mysourcedata==392]<-"Japan"
+mysourcedata[mysourcedata==398]<-"Kazakhstan"
+mysourcedata[mysourcedata==400]<-"Jordan"
+mysourcedata[mysourcedata==404]<-"Kenya"
+mysourcedata[mysourcedata==408]<-"North Korea"
+mysourcedata[mysourcedata==410]<-"South Korea"
+mysourcedata[mysourcedata==414]<-"Kuwait"
+mysourcedata[mysourcedata==417]<-"Kyrgyzstan"
+mysourcedata[mysourcedata==418]<-"Laos"
+mysourcedata[mysourcedata==422]<-"Lebanon"
+mysourcedata[mysourcedata==426]<-"Lesotho"
+mysourcedata[mysourcedata==428]<-"Latvia"
+mysourcedata[mysourcedata==430]<-"Liberia"
+mysourcedata[mysourcedata==434]<-"Libya"
+mysourcedata[mysourcedata==438]<-"Liechtenstein"
+mysourcedata[mysourcedata==440]<-"Lithuania"
+mysourcedata[mysourcedata==442]<-"Luxembourg"
+mysourcedata[mysourcedata==450]<-"Madagascar"
+mysourcedata[mysourcedata==454]<-"Malawi"
+mysourcedata[mysourcedata==458]<-"Malaysia"
+mysourcedata[mysourcedata==466]<-"Mali"
+mysourcedata[mysourcedata==470]<-"Malta"
+mysourcedata[mysourcedata==474]<-"Martinique"
+mysourcedata[mysourcedata==478]<-"Mauritania"
+mysourcedata[mysourcedata==480]<-"Mauritius"
+mysourcedata[mysourcedata==484]<-"Mexico"
+mysourcedata[mysourcedata==492]<-"Monaco"
+mysourcedata[mysourcedata==496]<-"Mongolia"
+mysourcedata[mysourcedata==498]<-"Moldova"
+mysourcedata[mysourcedata==504]<-"Morocco"
+mysourcedata[mysourcedata==508]<-"Mozambique"
+mysourcedata[mysourcedata==512]<-"Oman"
+mysourcedata[mysourcedata==516]<-"Namibia"
+mysourcedata[mysourcedata==524]<-"Nepal"
+mysourcedata[mysourcedata==528]<-"Netherlands"
+mysourcedata[mysourcedata==554]<-"New Zealand"
+mysourcedata[mysourcedata==558]<-"Nicaragua"
+mysourcedata[mysourcedata==562]<-"Niger"
+mysourcedata[mysourcedata==566]<-"Nigeria"
+mysourcedata[mysourcedata==578]<-"Norway"
+mysourcedata[mysourcedata==586]<-"Pakistan"
+mysourcedata[mysourcedata==591]<-"Panama"
+mysourcedata[mysourcedata==598]<-"Papua New Guinea"
+mysourcedata[mysourcedata==600]<-"Paraguay"
+mysourcedata[mysourcedata==604]<-"Peru"
+mysourcedata[mysourcedata==608]<-"Philippines"
+mysourcedata[mysourcedata==616]<-"Poland"
+mysourcedata[mysourcedata==620]<-"Portugal"
+mysourcedata[mysourcedata==624]<-"Guinea-Bissau"
+mysourcedata[mysourcedata==626]<-"Timor-Leste"
+mysourcedata[mysourcedata==630]<-"Puerto Rico"
+mysourcedata[mysourcedata==634]<-"Qatar"
+mysourcedata[mysourcedata==642]<-"Romania"
+mysourcedata[mysourcedata==643]<-"Russia"
+mysourcedata[mysourcedata==646]<-"Rwanda"
+mysourcedata[mysourcedata==682]<-"Saudi Arabia"
+mysourcedata[mysourcedata==686]<-"Senegal"
+mysourcedata[mysourcedata==690]<-"Seychelles"
+mysourcedata[mysourcedata==694]<-"Sierra Leone"
+mysourcedata[mysourcedata==702]<-"Singapore"
+mysourcedata[mysourcedata==703]<-"Slovakia"
+mysourcedata[mysourcedata==704]<-"Viet Nam"
+mysourcedata[mysourcedata==705]<-"Slovenia"
+mysourcedata[mysourcedata==706]<-"Somalia"
+mysourcedata[mysourcedata==710]<-"South Africa"
+mysourcedata[mysourcedata==716]<-"Zimbabwe"
+mysourcedata[mysourcedata==724]<-"Spain"
+mysourcedata[mysourcedata==736]<-"Sudan"
+mysourcedata[mysourcedata==740]<-"Suriname"
+mysourcedata[mysourcedata==752]<-"Sweden"
+mysourcedata[mysourcedata==756]<-"Switzerland"
+mysourcedata[mysourcedata==760]<-"Syria"
+mysourcedata[mysourcedata==762]<-"Tajikistan"
+mysourcedata[mysourcedata==764]<-"Thailand"
+mysourcedata[mysourcedata==768]<-"Togo"
+mysourcedata[mysourcedata==780]<-"Trinidad and Tobago"
+mysourcedata[mysourcedata==784]<-"United Arab Emirates"
+mysourcedata[mysourcedata==788]<-"Tunisia"
+mysourcedata[mysourcedata==792]<-"Turkey"
+mysourcedata[mysourcedata==795]<-"Turkmenistan"
+mysourcedata[mysourcedata==800]<-"Uganda"
+mysourcedata[mysourcedata==804]<-"Ukraine"
+mysourcedata[mysourcedata==807]<-"Macedonia"
+mysourcedata[mysourcedata==818]<-"Egypt"
+mysourcedata[mysourcedata==826]<-"Great Britain"
+mysourcedata[mysourcedata==834]<-"Tanzania"
+mysourcedata[mysourcedata==840]<-"United States"
+mysourcedata[mysourcedata==850]<-"U.S. Virgin Islands"
+mysourcedata[mysourcedata==854]<-"Burkina Faso"
+mysourcedata[mysourcedata==858]<-"Uruguay"
+mysourcedata[mysourcedata==860]<-"Uzbekistan"
+mysourcedata[mysourcedata==862]<-"Venezuela"
+mysourcedata[mysourcedata==887]<-"Yemen"
+mysourcedata[mysourcedata==891]<-"Serbia and Montenegro"
+mysourcedata[mysourcedata==894]<-"Zambia"
+mysourcedata[mysourcedata==900]<-"West Germany"
+mysourcedata[mysourcedata==901]<-"East Germany"
+mysourcedata[mysourcedata==902]<-"Tambov"
+mysourcedata[mysourcedata==903]<-"Moscow"
+mysourcedata[mysourcedata==904]<-"Basque Country"
+mysourcedata[mysourcedata==906]<-"Andalusia"
+mysourcedata[mysourcedata==907]<-"Galicia"
+mysourcedata[mysourcedata==909]<-"North Ireland"
+mysourcedata[mysourcedata==910]<-"Valencia"
+mysourcedata[mysourcedata==911]<-"Serbia"
+mysourcedata[mysourcedata==912]<-"Montenegro"
+mysourcedata[mysourcedata==913]<-"Srpska Republic"
+mysourcedata[mysourcedata==914]<-"Bosnian Federation"
+mysourcedata[mysourcedata==915]<-"Kosovo"
+
+mysourcedata$LEARNRAW <- ((mysourcedata$V70/6)+(mysourcedata$V13/2)+(mysourcedata$V15/2)+(mysourcedata$V182/4)+(mysourcedata$V6/4)+(mysourcedata$V8/4))/6
+
+mysourcedata$BONDRAW <- ((mysourcedata$V74/6)+(mysourcedata$V74B/6)+(mysourcedata$V75/6)+(mysourcedata$V79/6)+(mysourcedata$V16/2)+(mysourcedata$V19/2)+(mysourcedata$V20/2)+(mysourcedata$V21/2)+(mysourcedata$V182/4)+(mysourcedata$V4/4)+(mysourcedata$V5/4)+(mysourcedata$V7/4)+(mysourcedata$V9/4))/13
+
+mysourcedata$DEFENDRAW <- ((mysourcedata$V72/6)+(mysourcedata$V77/6)+(mysourcedata$V78/6)+(mysourcedata$V12/2)+(mysourcedata$V14/2)+(mysourcedata$V66/2)+(mysourcedata$V7/4))/7
+
+mysourcedata$ACQUIRERAW <- ((mysourcedata$V71/6)+(mysourcedata$V75/6)+(mysourcedata$V13/2)+(mysourcedata$V17/2)+(mysourcedata$V18/2)+(mysourcedata$V8/4))/6
+
+mysourcedata$FEELRAW <-((mysourcedata$V73/6)+(mysourcedata$V74/6)+(mysourcedata$V74B/6)+(mysourcedata$V76/6)+(mysourcedata$V19/2)+(mysourcedata$V22/2)+(mysourcedata$V9/4))/7
+
+mysourcedata$LEARN<- mysourcedata$LEARNRAW/(mysourcedata$LEARNRAW + mysourcedata$BONDRAW + mysourcedata$DEFENDRAW + mysourcedata$ACQUIRERAW + mysourcedata$FEELRAW)
+
+mysourcedata$BOND<- mysourcedata$BONDRAW/(mysourcedata$LEARNRAW + mysourcedata$BONDRAW + mysourcedata$DEFENDRAW + mysourcedata$ACQUIRERAW + mysourcedata$FEELRAW)
+
+mysourcedata$DEFEND<- mysourcedata$DEFENDRAW/(mysourcedata$LEARNRAW + mysourcedata$BONDRAW + mysourcedata$DEFENDRAW + mysourcedata$ACQUIRERAW + mysourcedata$FEELRAW)
+
+mysourcedata$ACQUIRE<- mysourcedata$ACQUIRERAW/(mysourcedata$LEARNRAW + mysourcedata$BONDRAW + mysourcedata$DEFENDRAW + mysourcedata$ACQUIRERAW + mysourcedata$FEELRAW)
+
+mysourcedata$FEEL<- mysourcedata$FEELRAW/(mysourcedata$LEARNRAW + mysourcedata$BONDRAW + mysourcedata$DEFENDRAW + mysourcedata$ACQUIRERAW + mysourcedata$FEELRAW)
+
+myscoredata<-mysourcedata[,c("V2","LEARN","BOND","DEFEND","ACQUIRE","FEEL")]
+
+ggplot(myscoredata, aes(x=LEARN, y=BOND, fill=DEFEND, color=ACQUIRE, size=FEEL^2)) +
+  geom_point(shape=21) +
+  scale_color_gradient(low="red", high="green") +
+  scale_size_continuous(range=c(1,12))
+  
+  
+mysourcedata$LEARN<-round(rescale(mysourcedata$LEARN, to=c(0,50)))
+mysourcedata$BOND<-round(rescale(mysourcedata$BOND, to=c(0,50)))
+mysourcedata$DEFEND<-round(rescale(mysourcedata$DEFEND, to=c(0,50)))
+mysourcedata$ACQUIRE<-round(rescale(mysourcedata$ACQUIRE, to=c(0,50)))
+mysourcedata$FEEL<-round(rescale(mysourcedata$FEEL, to=c(0,50)))
+
+mydata<-mysourcedata[,c(LEARN,BOND,DEFEND,ACQUIRE,FEEL)]
+
+wssplot <- function(data, nc=15, seed=1234){
+   wss <- (nrow(data)-1)*sum(apply(data,2,var))
+   for (i in 2:nc){
+     set.seed(seed)
+     wss[i] <- sum(kmeans(data, centers=i, iter.max=300 , algorithm="Lloyd")$withinss)}
+   plot(1:nc, wss, type="b", xlab="Number of Clusters",
+        ylab="Within groups sum of squares")}
+
+
+wssplot(mydata,nc=20)
+
+k.means.fit <- kmeans(mydata, [INSERT CLUSTER AMOUNT])
+mycenters<-as.data.frame(k.means.fit$centers)
+  
+ggplot(myscoredata,aes(x=ACQUIRE,y=LEARN)) + stat_binhex()
+ggplot(myscoredata,aes(x=ACQUIRE,y=BOND)) + stat_binhex()
+ggplot(myscoredata,aes(x=ACQUIRE,y=DEFEND)) + stat_binhex()
+ggplot(myscoredata,aes(x=ACQUIRE,y=FEEL)) + stat_binhex()
+
+myscoredata$LEARN<-round(rescale(myscoredata$LEARN, to=c(0,100)))
+myscoredata$BOND<-round(rescale(myscoredata$BOND, to=c(0,100)))
+myscoredata$DEFEND<-round(rescale(myscoredata$DEFEND, to=c(0,100)))
+myscoredata$ACQUIRE<-round(rescale(myscoredata$ACQUIRE, to=c(0,100)))
+myscoredata$FEEL<-round(rescale(myscoredata$FEEL, to=c(0,100)))
+countdata<-ddply(myscoredata,.(LEARN,BOND,DEFEND,ACQUIRE,FEEL),nrow)
+
+ggplot(countdata, aes(x=LEARN, y=BOND, fill=DEFEND, color=ACQUIRE, size=V1)) +
+  geom_point(shape=21) +
+  scale_color_gradient(low="red", high="green") +
+  scale_size_continuous(range=c(1,12))
+  
